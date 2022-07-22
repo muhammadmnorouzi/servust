bda9b9b0b9d6aff5153fb890036b1c88f71fb229
Muhammad.M Norouzi <Muhammadmnorouzi@Outlook.com>
2022-07-22 18:53:58 +0430

    using modules to organize things together.

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,11 @@
+use http::Method;
+use http::Request;
+use server::Server;
+
+mod http;
+mod server;
+
 fn main() {
     let server = Server::new("127.0.0.1:8080".to_string());
     server.run();
-}
-
-struct Server {
-    address: String,
-}
-
-impl Server {
-    fn new(addr: String) -> Self {
-        Self { address: addr }
-    }
-
-    fn run(self) {}
-}
-
-struct HttpRequest {
-    path: String,
-    query_string: String,
-    method: HttpMethod,
-}
-
-enum HttpMethod {
-    GET,
-    DELETE,
-    POST,
-    PUT,
-    HEAD,
-    CONNECT,
-    OPTIONS,
-    TRACE,
-    PATCH,
 }
---
+++ src/server.rs
@@ -0,0 +1,13 @@
+pub struct Server {
+    address: String,
+}
+
+impl Server {
+    pub fn new(addr: String) -> Self {
+        Self { address: addr }
+    }
+
+    pub fn run(self) {
+        println!("listening on {:}", self.address);
+    }
+}

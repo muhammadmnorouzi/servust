4c1c22068d37e421061ba6a9089726a0063cd3bd
Muhammad.M Norouzi <Muhammadmnorouzi@Outlook.com>
2022-07-28 16:07:20 +0430

    using files and env var

--- src/website_handler.rs
+++ src/website_handler.rs
@@ -3,14 +3,42 @@ use crate::{
     server::HttpRequestHandler,
 };
 
-pub struct WebsiteHandler;
+pub struct WebsiteHandler {
+    public_path: String,
+}
+
+impl WebsiteHandler {
+    pub fn new(public_path: String) -> Self {
+        Self { public_path }
+    }
+}
 
 impl HttpRequestHandler for WebsiteHandler {
-    fn handle_request(&mut self, _request: &crate::http::Request) -> crate::http::Response {
-        Response::new(
-            HttpStatusCode::Ok,
-            Some("<title>This is the title</title><h1>This is a test</h1>".to_string()),
-        )
+    fn handle_request(&mut self, request: &crate::http::Request) -> crate::http::Response {
+        match request.method() {
+            crate::http::Method::GET => match request.path() {
+                "/" => Response::new(
+                    HttpStatusCode::Ok,
+                    Some("<h1>Welcome to root page</h1>".to_string()),
+                ),
+                "/hello" => Response::new(
+                    HttpStatusCode::Ok,
+                    Some("<h1>Welcome to Hello page</h1>".to_string()),
+                ),
+                _ => Response::new(
+                    HttpStatusCode::NotFound,
+                    Some("<h1>Page not found</h1>".to_string()),
+                ),
+            },
+            crate::http::Method::DELETE => todo!(),
+            crate::http::Method::POST => todo!(),
+            crate::http::Method::PUT => todo!(),
+            crate::http::Method::HEAD => todo!(),
+            crate::http::Method::CONNECT => todo!(),
+            crate::http::Method::OPTIONS => todo!(),
+            crate::http::Method::TRACE => todo!(),
+            crate::http::Method::PATCH => todo!(),
+        }
     }
 
     // fn handle_bad_request(&mut self, error: &crate::http::ParsingError) -> crate::http::Response {
